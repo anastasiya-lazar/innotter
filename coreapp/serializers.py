eb67bc4c2f069db668ebf1c1b198b98e187f89fd
lelonea <leloneya@gmail.com>
2023-02-15 12:04:27 +0100

    Added serializers and expanded viewsets

--- coreapp/serializers.py
+++ coreapp/serializers.py
@@ -4,51 +4,48 @@ from django.core import exceptions
 import django.contrib.auth.password_validation as validators
 from coreapp.models import User, Page, Post, Tag
 
-class UserModelSerializer(serializers.ModelSerializer):
+class UserListModelSerializer(serializers.ModelSerializer):
     """
-    A Serializer for User model with implementations of 'validate()' - for password validation and 'create()' methods
+    A Serializer for User model (list action)
     """
     class Meta:
         model = User
-        fields = ["id","username", "email", "first_name", "last_name", "password"]
-    extra_kwargs = {'password': {'write_only': True,}}
+        fields = ["first_name", "last_name", "username"]
+
+
+class UserModelSerializer(serializers.ModelSerializer):
+    """
+    A Default Serializer for User model with implementations of 'validate()' - for password validation and 'create()' methods
+    """
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "first_name", "last_name", "title", "password"]
+        extra_kwargs = {"password": {"write_only": True,},
+                        "email":{"write_only": True,}
+        }
 
     def validate(self, data):
          user = User(**data)
-         password = data.get('password')
+         password = data.get("password")
          errors = dict() 
          try:
              validators.validate_password(password=password, user=user)
          except exceptions.ValidationError as e:
-             errors['password'] = list(e.messages)
+             errors["password"] = list(e.messages)
          if errors:
              raise serializers.ValidationError(errors)
          return super(UserModelSerializer, self).validate(data)
-
+    
     def create(self, validated_data):
-        user = User(
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            username=validated_data['username'],
-            password = make_password(validated_data['password'],)
-        )
-        user.save()
-        return user
-
-
-class PageModelSerializer(serializers.ModelSerializer):
-    """
-    A Serializer for Page model with implementation of 'create()' method
-    """
-    class Meta:
-        model = Page
-        fields = ["name", "description", "image", "is_private", "owner", "tags", "unblock_date", "followers", "follow_requests"]
-        read_only_fields = ['owner',  "unblock_date", "followers", "follow_requests"]
-
-    def create(self, validated_data):
-        validated_data['owner'] = self.context['request'].user
+        validated_data["password"] = make_password(validated_data["password"],)
         return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        user = super().update(instance, validated_data)
+        if "password" in validated_data:
+            user.set_password(validated_data["password"])
+            user.save()
+        return user
 
 
 class TagModelSerializer(serializers.ModelSerializer):
@@ -57,14 +54,101 @@ class TagModelSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Tag
-        fields = ["name"]
+        fields = ["id", "name"]
+        read_only_fields = ["id"]
+
+
+class PageModelSerializer(serializers.ModelSerializer):
+    """
+    A Default Serializer for Page model with implementation of 'create()' method
+    """
+    class Meta:
+        model = Page
+        fields = ["name", "description", "image", "is_private", "owner", "tags"]
+        read_only_fields = ["owner"]
+
+    def create(self, validated_data):
+        validated_data["owner"] = self.context["request"].user
+        return super().create(validated_data)
+
+class PageListModelSerializer(serializers.ModelSerializer):
+    """
+    A Serializer for Page model (list action)
+    """
+    tags = TagModelSerializer(many = True)
+    owner = UserModelSerializer()
+    class Meta:
+        model = Page
+        fields = ["id", "name", "description", "image", "owner", "is_private", "tags"]
+
+
+class PageRetrieveModelSerializer(serializers.ModelSerializer):
+    """
+    A Serializer for Page model (retrieve action)
+    """
+    tags = TagModelSerializer(many = True)
+    owner = UserModelSerializer()
+    class Meta:
+        model = Page
+        fields = ["id", "name", "description", "image", "owner", "is_private", "tags", "followers", "follow_requests"]
+
+
+class PageUpdateModelSerializer(serializers.ModelSerializer):
+    """
+    A Serializer for Page model (update action)
+    """
+    class Meta:
+        model = Page
+        fields = ["name", "description", "image", "is_private", "tags", "followers", "follow_requests"]
+
+
+class PageOwnerModelSerializer(serializers.ModelSerializer):
+    """
+    A Serializer for Page model (owner)
+    """
+    owner = UserListModelSerializer()
+    class Meta:
+        model = Page
+        fields = ["owner"]
+
+
+class UserRetrieveModelSerializer(serializers.ModelSerializer):
+    """
+    A Serializer for User model (retrieve action)
+    """
+    pages = PageModelSerializer(many = True)
+    class Meta:
+        model = User
+        fields = ["id", "username", "first_name", "last_name", "title", "date_joined", "pages"]
 
 
 class PostModelSerializer(serializers.ModelSerializer):
     """
-    A Serializer for Post model
+    A Default Serializer for Post model
     """
     class Meta:
         model = Post
-        fields = ["content", "page", 'reply_to']
+        fields = ["id", "content", "page", "reply_to", "created_at", "updated_at"]
+        read_only_fields = ["id", "created_at", "updated_at"]
+        
+
+class PostRetrieveModelSerializer(serializers.ModelSerializer):
+    """
+    A Serializer for Post model (retrieve action)
+    """
+    page = PageOwnerModelSerializer()
+    reply_to = PostModelSerializer()
+    class Meta:
+        model = Post
+        read_only_fields = ["id", "created_at", "updated_at"]
+        fields = ["id", "content", "page", "reply_to", "created_at", "updated_at"]
+
+
+class PostUpdateModelSerializer(serializers.ModelSerializer):
+    """
+    A Serializer for Post model (update action)
+    """
+    class Meta:
+        model = Post
+        fields = ["content"]
 
--- coreapp/views.py
+++ coreapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions, mixins
 from coreapp.models import User, Page, Tag, Post 
-from coreapp.serializers import UserModelSerializer, PageModelSerializer, TagModelSerializer, PostModelSerializer
+from coreapp import serializers
 
 
 class UserViewSet(mixins.CreateModelMixin,
@@ -11,11 +11,19 @@ class UserViewSet(mixins.CreateModelMixin,
                   mixins.ListModelMixin,
                   viewsets.GenericViewSet):
     """
-    A viewset for instances of User model that provides `create()`, `retrieve()`, `update()`,
+    A viewset for instances of User model that provides `create()`, `retrieve()`,
     `partial_update()`, `destroy()` and `list()` actions.
     """
     queryset = User.objects.all()
-    serializer_class = UserModelSerializer
+    http_method_names = ('get', 'post', 'patch', 'delete')
+    serializer_classes = { 
+        'list': serializers.UserListModelSerializer,
+        'retrieve': serializers.UserRetrieveModelSerializer,
+        'default':  serializers.UserModelSerializer,
+    }
+
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action, self.serializer_classes ['default'])
 
 
 class PageViewSet(mixins.CreateModelMixin,
@@ -25,28 +33,35 @@ class PageViewSet(mixins.CreateModelMixin,
                   mixins.ListModelMixin,
                   viewsets.GenericViewSet):
     """
-    A viewset for instances of Page model that provides `create()`, `retrieve()`, `update()`,
+    A viewset for instances of Page model that provides `create()`, `retrieve()`,
     `partial_update()`, `destroy()` and `list()` actions.
     """
     queryset = Page.objects.all()
-    serializer_class = PageModelSerializer
+    serializer_class = serializers.PageModelSerializer
     permission_classes = [
         permissions.IsAuthenticated
     ]
+    http_method_names = ('get', 'post', 'patch', 'delete')
+    serializer_classes = { 
+        'partial_update': serializers.PageUpdateModelSerializer,
+        'list': serializers.PageListModelSerializer,
+        'retrieve': serializers.PageRetrieveModelSerializer,
+        'default':  serializers.PageModelSerializer,
+    }
+
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action, self.serializer_classes ['default'])
 
 
 class TagViewSet(mixins.CreateModelMixin,
-                  mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  mixins.DestroyModelMixin,
-                  mixins.ListModelMixin,
-                  viewsets.GenericViewSet):
+                mixins.DestroyModelMixin,
+                mixins.ListModelMixin,
+                viewsets.GenericViewSet):
     """
-    A viewset for instances of Tag model that provides `create()`, `retrieve()`, `update()`,
-    `partial_update()`, `destroy()` and `list()` actions.
+    A viewset for instances of Tag model that provides `create()`, `destroy()` and `list()` actions.
     """
     queryset = Tag.objects.all()
-    serializer_class = TagModelSerializer
+    serializer_class = serializers.TagModelSerializer
 
 
 class PostViewSet(mixins.CreateModelMixin,
@@ -56,9 +71,20 @@ class PostViewSet(mixins.CreateModelMixin,
                   mixins.ListModelMixin,
                   viewsets.GenericViewSet):
     """
-    A viewset for instances of Post model that provides `create()`, `retrieve()`, `update()`,
-    `partial_update()`, `destroy()` and `list()` actions.
+    A viewset for instances of Post model that provides `create()`, `retrieve()`, `partial_update()`, `destroy()` and `list()` actions.
     """
     queryset = Post.objects.all()
-    serializer_class = PostModelSerializer
+   
+    http_method_names = ('get', 'post', 'patch', 'delete')
+
+    serializer_classes = { 
+        'partial_update': serializers.PostUpdateModelSerializer,
+        'retrieve': serializers.PostRetrieveModelSerializer,
+        'default':  serializers.PostModelSerializer,
+    }
+
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action, self.serializer_classes ['default'])
+
+
 
